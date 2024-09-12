93fc00ae6d487675dbfc3c5178553a1096f81041
Ninzato <ridzakalimanto@gmail.com>
2024-09-12 16:57:59 +0700

    Add Usercourses feature:

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,16 +1,16 @@
 const { Course } = require("../models");
 
 class Controller {
-  static async home(req,res){
-    const {search} = req.query;
+  static async home(req, res) {
+    const { search } = req.query;
     try {
-        const courses = await Course.findAll();
-        res.render("Home",{courses});
-    } catch (err){
-        console.log(err.message);
-        res.send(err.message);
+      const courses = await Course.findAll();
+      res.render("Home", { courses });
+    } catch (err) {
+      console.log(err.message);
+      res.send(err.message);
     }
-}
+  }
 }
 
 module.exports = Controller;
--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,19 @@ router.post("/signUp", RidzaController.signUpPost);
 router.get("/signIn", RidzaController.signInForm);
 router.post("/signIn", RidzaController.signInPost);
 
+// router.use((req, res, next) => {
+//   if (!req.session.userId) {
+//     res.redirect("/signIn?errors=Please login first!"); //kalau user id gada di session, tendang balik ke login page
+//   } else {
+//     next(); //udah login? monggo lanjut
+//   }
+// });
+
+// UserProfile
+router.get("/userProfile", RidzaController.userProfileForm);
+router.post("/userProfile", RidzaController.userProfilePost);
+
+// UserCourses
+router.get("/userCourses", RidzaController.userCourses);
+
 module.exports = router;
