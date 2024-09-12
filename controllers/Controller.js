b442fa44440804ebd75859039d6b433db8475abd
erica olivia <ecaoliviaaa@gmail.com>
2024-09-12 12:36:28 +0800

    Navbar temp

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,12 +1,16 @@
+const { Course } = require("../models");
+
 class Controller {
-  static async home(req, res) {
+  static async home(req,res){
+    const {search} = req.query;
     try {
-      res.send("home");
-    } catch (err) {
-      console.log(err);
-      res.send(err.message);
+        const courses = await Course.findAll();
+        res.render("Home",{courses});
+    } catch (err){
+        console.log(err.message);
+        res.send(err.message);
     }
-  }
+}
 }
 
 module.exports = Controller;
